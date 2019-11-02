f0dd221d8f85dc008ce384a3692ac81882b04fe4
Elliott Clark <eclark@apache.org>
2019-11-02 00:57:02 -0700

    Add on a benchmark

---
+++ benches/rps.rs
@@ -0,0 +1,21 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+
+use std::time::Instant;
+
+use little_sorry::rps::RPSRunner;
+
+pub fn bench(c: &mut Criterion) {
+    c.bench_function("iter", move |b| {
+        b.iter_custom(|iters| {
+            let mut runner = RPSRunner::new();
+            let start = Instant::now();
+            for _i in 0..iters {
+                runner.run_one();
+            }
+            start.elapsed()
+        })
+    });
+}
+
+criterion_group!(benches, bench);
+criterion_main!(benches);
